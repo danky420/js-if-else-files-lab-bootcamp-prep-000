07f309a0a56fced8a3c0759c56e5ce875ea81ced
Frank Calvario <frankcalvario@gmail.com>
2018-05-17 02:56:18 +0000

    automatically backed up by learn

--- tests.js
+++ tests.js
@@ -1,8 +1,3 @@
-var index = require("./index.js")
-
-if (index.name === "Susan") {
-    console.log("The name is correct")
-} else {
     console.log("Expected: Susan, Received: "+index.name)
 }
 
@@ -11,4 +6,8 @@ if (index.name === "Susan") {
 if (index.height === 70) {
     console.log("The height is correct")
 
-}
+}var index = require("./index.js")
+
+if (index.name === "Susan") {
+    console.log("The name is correct")
+} else {
